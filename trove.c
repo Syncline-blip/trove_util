983125c95b3bfc7143b53d4234dc8a1433bf36e6
John Lumagbas <jadlumagbas@gmail>
2022-10-13 14:25:26 +0800

    fixed terminal arguments

--- trove.c
+++ trove.c
@@ -7,7 +7,10 @@
 #include <errno.h>
 #include <ctype.h>
 #include <time.h>
-
+#include <unistd.h> 
+#include <getopt.h>
+#define DEFAULT_DIR "/tmp/trove"
+#define DEFAULT_LEN 4
 // usage()
 void usage()
 {   
@@ -28,5 +31,78 @@ int main(int argc, char *argv[])
     
         readTrovefile(argv[1], argv[2]);
         
+<<<<<<< HEAD
+=======
+=======
+    int options, inputLength;
+    
+        while (optind <argc)
+        {
+            
+            if ((options = getopt(argc,argv, "f:brul:")) != -1)
+            {
+                    switch(options)
+                    {
+                        case 'f': {
+                            
+                            if(optarg == NULL)
+                            {
+                                printf("hentai : %s", optarg);
+                                exit(EXIT_FAILURE);
+                            }
+                            printf("file name: %s\n", optarg);
+                            break;
+                        }
+                        case 'b':
+                            printf("case b success\n");
+                            break;
+                        case 'r':
+                            printf("case r success\n");
+                            break;
+                        case 'u':
+                            printf("case u success\n");
+                            break;
+                        case 'l':
+                                            {
+                            inputLength = (int)atol(optarg);
+                            if(inputLength == 0)
+                            {
+                                printf("-l error: length cannot 0\n");
+                                exit(EXIT_FAILURE);
+                            }
+                            
+                            printf("length spec:     %d\n", inputLength);   
+                            break;
+                        }
+                        default:
+                            usage();
+                            exit(EXIT_FAILURE);
+                            break;
+                    }
+            }
+            else
+            {
+                if(argc == 2)
+                {
+                    printf("%s", argv[1]);
+                }
+                else if(argc == 4)
+                {
+                    printf("%s", argv[3]);
+                }
+                optind++;
+            }
+        }
+        
+    
+
+    
+    
+    // }
+    
+    // list_directory(argv[1]);
+    // printf("\n");
+    // printf("Test if word 'HASHTABLE_SIZE' exists in hashtable.c\n");
+>>>>>>> 3da9942 (fixed terminal arguments)
 
 }
